642dc3b311d3a299e79e2ab7241b73d58414ec76
N.Sai Neethu <saineethunalla112@gmail.comgit config --global user.email saineethunalla112@gmail.com>
2025-06-14 19:15:55 +0530

    Added trailers

--- ownProject.js
+++ ownProject.js
@@ -1,11 +1,20 @@
 
 let image=document.querySelectorAll("img");
 
-image.forEach((image)=>{   
-image.addEventListener("click", (e) =>{
-    let movieLink = document.createElement("a");
-    movieLink.href="https://www.youtube.com/";
-    movieLink.target="_blank";
-    movieLink.click();
-});
-});
+// image.forEach((image)=>{   
+// image.addEventListener("click", (e) =>{
+//     let movieLink = document.createElement("a");
+//     if(id="anaganaga"){
+//         movieLink.href="https://www.youtube.com/watch?v=WH7kqm_4rdU";
+//         movieLink.target="_blank";
+//         movieLink.click();
+//     }
+//     else if(id="hit3"){
+//         movieLink.href="https://www.youtube.com/watch?v=kAtfaaUgDRU&t=2s";
+//         movieLink.target="_blank";
+//         movieLink.click();
+//     }
+    
+// });
+// });
+
