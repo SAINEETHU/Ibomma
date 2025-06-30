1c608033a15956ac8c10e70317a7d635ee676e9e
N.Sai Neethu <saineethunalla112@gmail.comgit config --global user.email saineethunalla112@gmail.com>
2025-06-30 11:38:51 +0530

    Committed changes in footer

--- ownProject.js
+++ ownProject.js
@@ -1,35 +1,14 @@
 
-let image=document.querySelectorAll("img");
-
-// image.forEach((image)=>{   
-// image.addEventListener("click", (e) =>{
-//     let movieLink = document.createElement("a");
-//     if(id="anaganaga"){
-//         movieLink.href="https://www.youtube.com/watch?v=WH7kqm_4rdU";
-//         movieLink.target="_blank";
-//         movieLink.click();
-//     }
-//     else if(id="hit3"){
-//         movieLink.href="https://www.youtube.com/watch?v=kAtfaaUgDRU&t=2s";
-//         movieLink.target="_blank";
-//         movieLink.click();
-//     }
-    
-// });
-// });
-
 let btns = document.querySelectorAll("#button");
-// let btns=document.getElementsByClassName("btn");
-
-// for(let btn of btns){
-// btn.addEventListener("click", ()=>{
-//     console.log("button was clicked");
-// })
-// };
-
 btns.forEach((btn)=>
  btn.addEventListener("click", ()=>{
      console.log("button was clicked");
-
  })
+);
+
+let images = document.getElementsByClassName("image-item");
+Array.from(images).forEach((image)=>
+    image.addEventListener("click",()=>{
+        console.log("image was clicked");
+    })
 );
